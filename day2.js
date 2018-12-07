da374504543051a7c1e93c37a53dd7300f0d259f
Jonathan Grim <jgrim@pindrop.com>
2018-12-07 12:56:41 -0500

    solution for part 1 of day 2

---
+++ day2.js
@@ -0,0 +1,27 @@
+const input = require('./day2_input');
+
+function getCountOfLetters([...letters]) {
+  const lettersHash = letters.reduce((acc, cur) => {
+    acc[cur] = acc[cur] ? acc[cur] + 1 : 1;
+    return acc;
+  }, {});
+  const letterCount = Object.values(lettersHash);
+  return [letterCount.indexOf(2), letterCount.indexOf(3)];
+}
+
+
+function loopThroughInputs(input) {
+  const inputs = input.split('\n');
+  let countOf2 = 0;
+  let countOf3 = 0;
+  inputs.forEach(i => {
+    const [exactly2, exactly3] = getCountOfLetters(i);
+    if (exactly2 >= 0) countOf2++;
+    if (exactly3 >= 0) countOf3++;
+  });
+
+  // assumes we'll have at least 1 hit on each
+  return countOf2 * countOf3;
+}
+
+console.log(loopThroughInputs(input));
